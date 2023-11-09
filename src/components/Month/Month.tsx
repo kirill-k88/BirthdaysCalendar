3c261207283f0b3ed5facf3fbed5ed1d7e7fc46b
kirill-k88 <kirill-k88@ya.ru>
2023-11-09 22:11:50 +0300

    features: add header and Month components

---
+++ src/components/Month/Month.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Month.scss';
+
+export function Month() {
+  return (
+    <section className="month">
+      <h2 className="month__header">Ноябрь</h2>
+      <div className="month__day-list">
+        <div>sdfdgsf</div>
+      </div>
+    </section>
+  );
+}
